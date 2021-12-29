0542b8fd7e592dcaf244b003825a15570608a756
ErikRobles <erikjames69@hotmail.com>
2021-12-28 22:11:03 -0600

    first commit

---
+++ src/pages/Favorites.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
+const Favorites = () => {
+  const favoritesCtx = useContext(FavoritesContext);
+  let content;
+  if (favoritesCtx.totalFavorites === 0) {
+    content = <p>You don't have any favorites yet. Please Add Some.</p>;
+  } else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+  }
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
+};
+
+export default Favorites;
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
+const NewMeetup = () => {
+  const navigate = useNavigate();
+  const addMeetupHandler = (meetupData) => {
+    fetch(
+      'https://reactmeetups-33121-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    ).then(() => {
+      navigate('/');
+    });
+  };
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+};
+
+export default NewMeetup;
